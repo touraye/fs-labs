b8ecaf03e3888b9ac5386a66ddf99cae8cf3dce4
touraye <touraye07@gmail.com>
2022-01-11 18:32:48 +0000

    clicking button in test

--- part4-sol/notes/src/components/Note.test.js
+++ part4-sol/notes/src/components/Note.test.js
@@ -1,32 +1,23 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent} from '@testing-library/react'
+
 import Note from './Note'
 
-test('renders content', () => {
+
+
+test('clicking the button calls event handler once', () => {
 	const note = {
 		content: 'Component testing is done with react-testing-library',
 		important: true,
 	}
 
-    const component = render( <Note note={ note } /> )
-    
-    component.debug()
+	const mockHandler = jest.fn()
 
-    //method 1
-	expect(component.container).toHaveTextContent(
-		'Component testing is done with react-testing-library'
-	)
+	const component = render(<Note note={note} toggleImportance={mockHandler} />)
 
-	// method 2
-	const element = component.getByText(
-		'Component testing is done with react-testing-library'
-	)
-	expect(element).toBeDefined()
+	const button = component.getByText('make not important')
+	fireEvent.click(button)
 
-	// method 3
-	const div = component.container.querySelector('.note')
-	expect(div).toHaveTextContent(
-		'Component testing is done with react-testing-library'
-	)
+	expect(mockHandler.mock.calls).toHaveLength(1)
 })
