f4f88c87b81defd6ecd662e13b2b73f3e7fcb1c0
touraye <touraye07@gmail.com>
2022-05-13 12:11:20 +0000

    changing phone number

--- part8-sol/graph-ql-phonebook-client/src/App.js
+++ part8-sol/graph-ql-phonebook-client/src/App.js
@@ -1,27 +1,41 @@
-import { gql, useQuery } from '@apollo/client'
+import { useState } from 'react'
+import { useQuery } from '@apollo/client'
+import PersonForm from './component/PersonForm'
 
 import Persons from './component/Persons'
+import { ALL_PERSONS } from './queries'
+import PhoneForm from './component/PhoneForm'
 
-const ALL_PERSONS = gql`
-	query {
-		allPersons {
-			name
-			phone
-			id
-		}
-	}
-`
 
 
 const App = () => {
-	const result = useQuery(ALL_PERSONS)
+	const [errorMessage, setErrorMessage] = useState(null)
+	
+	const result = useQuery( ALL_PERSONS )
 
 	if (result.loading) {
 		return <div>loading...</div>
 	}
 
+	  const notify = (message) => {
+			setErrorMessage(message)
+			setTimeout(() => {
+				setErrorMessage(null)
+			}, 10000)
+		}
+	
+	const Notify = ({ errorMessage }) => {
+		if (!errorMessage) {
+			return null
+		}
+		return <div style={{ color: 'red' }}>{errorMessage}</div>
+	}
+
 	return (
 		<div>
+			<Notify errorMessage={errorMessage} />
+			<PersonForm setError={ notify } />
+			<PhoneForm />
 			<Persons persons={result.data.allPersons} />
 		</div>
 	)
--- part8-sol/graph-ql-phonebook-client/src/component/Persons.jsx
+++ part8-sol/graph-ql-phonebook-client/src/component/Persons.jsx
@@ -1,21 +1,8 @@
-import { gql, useQuery } from '@apollo/client'
+import { useQuery } from '@apollo/client'
 import { useState } from 'react'
 
 import Person from './Person'
-
-const FIND_PERSON = gql`
-	query findPersonByName($nameToSearch: String!) {
-		findPerson(name: $nameToSearch) {
-			name
-			phone
-			id
-			address {
-				street
-				city
-			}
-		}
-	}
-`
+import { FIND_PERSON } from '../queries'
 
 const Persons = ( { persons } ) => {
   const [nameToSearch, setNameToSearch] = useState(null)
